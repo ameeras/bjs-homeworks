ff325c02ca776201e8f4cb3f584c9b83400bc6e7
Yuliya Pletneva <shamekeshova@gmail.com>
2019-01-03 20:32:33 +0600

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,26 @@
+// task 1
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+// task 2
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let average = (algebra + geography + physics) / 3;
+
+console.log(`Средний балл - ${average}`);
+
+// task 3
+
+let name = 'Юлия';
+let message = `Привет, мир! Меня зовут ${name}!`;
+
+console.log(message);
